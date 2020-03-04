dffa977500c09fc3699ab39eaba724ac53b486b6
vyou <vyou@umich.edu>
2020-03-04 03:06:43 -0500

    finished camera control

--- Assets/Scripts/BoundaryManager.cs
+++ Assets/Scripts/BoundaryManager.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoundaryManager : MonoBehaviour
-{
+{   
+    private BoxCollider2D managerBox;
+    private Transform player;
+    public GameObject boundary;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        managerBox = GetComponent<BoxCollider2D> ();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform> ();
+        //source = audio.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageBoundary ();
+    }
+
+    void ManageBoundary ()
+    {
+        if (managerBox.bounds.min.x < player.position.x && player.position.x < managerBox.bounds.max.x &&
+            managerBox.bounds.min.y < player.position.y && player.position.y < managerBox.bounds.max.y) {
+                boundary.SetActive (true);
+            }
+            else {
+                boundary.SetActive (false);
+            }
     }
 }
