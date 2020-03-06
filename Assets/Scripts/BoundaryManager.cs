318e2602ccc7790299fd730c4f65da796c27e666
vyou <vyou@umich.edu>
2020-03-06 17:00:13 -0500

    now the player object in which the camera follows can be edited

--- Assets/Scripts/BoundaryManager.cs
+++ Assets/Scripts/BoundaryManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class BoundaryManager : MonoBehaviour
 {   
     private BoxCollider2D managerBox;
-    private Transform player;
+    private Transform player_transform;
     public GameObject boundary;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         managerBox = GetComponent<BoxCollider2D> ();
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform> ();
+        player_transform = player.GetComponent<Transform> ();
         //source = audio.GetComponent<AudioSource>();
     }
 
@@ -24,8 +25,8 @@ public class BoundaryManager : MonoBehaviour
 
     void ManageBoundary ()
     {
-        if (managerBox.bounds.min.x < player.position.x && player.position.x < managerBox.bounds.max.x &&
-            managerBox.bounds.min.y < player.position.y && player.position.y < managerBox.bounds.max.y) {
+        if (managerBox.bounds.min.x < player_transform.position.x && player_transform.position.x < managerBox.bounds.max.x &&
+            managerBox.bounds.min.y < player_transform.position.y && player_transform.position.y < managerBox.bounds.max.y) {
                 boundary.SetActive (true);
             }
             else {
