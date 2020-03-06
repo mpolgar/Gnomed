1bd834a96b80b2b566037391f61d1e49b8fb8c36
vanessayyou <vyou@umich.edu>
2020-03-06 17:41:38 -0500

    Vyou zappy monster sprite (#2)
    * Finished organ animation of the zappy monster
    
    * finished walking animation
    
    * General structure of animation done
    
    * finished camera control
    
    * now the player object in which the camera follows can be edited
    
    Co-authored-by: MichaelBrazeau <michaelgbrazeau@gmail.com>

---
+++ Assets/Scripts/BoundaryManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryManager : MonoBehaviour
+{   
+    private BoxCollider2D managerBox;
+    private Transform player_transform;
+    public GameObject boundary;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        managerBox = GetComponent<BoxCollider2D> ();
+        player_transform = player.GetComponent<Transform> ();
+        //source = audio.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageBoundary ();
+    }
+
+    void ManageBoundary ()
+    {
+        if (managerBox.bounds.min.x < player_transform.position.x && player_transform.position.x < managerBox.bounds.max.x &&
+            managerBox.bounds.min.y < player_transform.position.y && player_transform.position.y < managerBox.bounds.max.y) {
+                boundary.SetActive (true);
+            }
+            else {
+                boundary.SetActive (false);
+            }
+    }
+}
