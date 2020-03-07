2fc9d9a4d4c5b5fea638566ec5ebf2f1b6dd6ad0
Matthew Polgar <mpolgar@umich.edu>
2020-03-07 03:25:27 -0500

    Movement code merged

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 using UnityEngine.InputSystem;
 
+public enum Actions
+{
+    MoveHorizontal,
+    MoveVertical,
+    JumpPressed,
+    JumpReleased,
+    Interact,
+};
+
+public class InputEvent
+{
+    public Actions action;
+    public float axis;
+
+    public InputEvent(Actions _in, float _axis)
+    {
+        action = _in;
+        axis = _axis;
+    }
+
+    public override string ToString()
+    {
+        return "Inputed " + action.ToString();
+    }
+}
+
 public class PlayerInput : MonoBehaviour
 {
-    public bool usingKeyboard = false;
+    
+    public const bool DEBUG_MODE = false;
 
     static PlayerInput instance;
-
-    Gamepad _gamepad;
 
     private void Awake()
     {
@@ -24,76 +49,40 @@ public class PlayerInput : MonoBehaviour
         {
             instance = this;
 
-            DontDestroyOnLoad(this.gameObject);
+            //DontDestroyOnLoad(this.gameObject);
         }
     }
-
-    void Update()
-    {
-        _gamepad = Gamepad.current;
-        if (_gamepad == null)
-        {
-            usingKeyboard = true;
-        }
-        else
-        {
-            usingKeyboard = false;
-        }
-
-        if (usingKeyboard)
-        {
-            float x = Input.GetAxis("Horizontal");
-            float y = Input.GetAxis("Vertical");
-            EventBus.Publish<InputEvent>(new InputEvent(Actions.Move, x, y));
-
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                EventBus.Publish<InputEvent>(new InputEvent(Actions.Jump, 0, 0));
-            }
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-                EventBus.Publish<InputEvent>(new InputEvent(Actions.Interact, 0, 0));
-            }
-
-        }
-        else
-        {
-            float x = _gamepad.leftStick.x.ReadValue();
-            float y = _gamepad.rightStick.y.ReadValue();
-            EventBus.Publish<InputEvent>(new InputEvent(Actions.Move, x, y));
-
-            if (_gamepad.aButton.wasPressedThisFrame)
-            {
-                EventBus.Publish<InputEvent>(new InputEvent(Actions.Jump, 0, 0));
-            }
-            if (_gamepad.bButton.wasPressedThisFrame)
-            {
-                EventBus.Publish<InputEvent>(new InputEvent(Actions.Interact, 0, 0));
-            }
+    
+    private void OnMoveHorizontal(InputValue value) {
+        float newValRaw = value.Get<float>();
+        float newVal = (newValRaw == 0f) ? 0f : Mathf.Sign(newValRaw);
+        DebugLog("moving horizontal " + newVal);
+        EventBus.Publish(new InputEvent(Actions.MoveHorizontal, newVal));
+    }
+    
+    private void OnMoveVertical(InputValue value) {
+        float newValRaw = value.Get<float>();
+        float newVal = (newValRaw == 0f) ? 0f : Mathf.Sign(newValRaw);
+        DebugLog("moving vertical " + newVal);
+        EventBus.Publish(new InputEvent(Actions.MoveVertical, newVal));
+    }
+    
+    private void OnJump(InputValue value) {
+        float val = value.Get<float>();
+        Actions status = (val == 0f) ? Actions.JumpReleased : Actions.JumpPressed;
+        DebugLog((val == 0f) ? "jump released" : "jump pressed");
+        EventBus.Publish(new InputEvent(status, 0f));
+    }
+    
+    private void OnInteract() {
+        DebugLog("interact");
+        EventBus.Publish(new InputEvent(Actions.Interact, 0f));
+    }
+    
+    private void DebugLog(string message) {
+        if (DEBUG_MODE) {
+            Debug.Log("PlayerInput: " + message);
         }
     }
-}
-
-public enum Actions
-{
-    Move,
-    Jump,
-    Interact,
-};
-
-public class InputEvent
-{
-    public Actions action;
-    public Vector2 axis;
-
-    public InputEvent(Actions _in, float x, float y)
-    {
-        action = _in;
-        axis = new Vector2(x, y);
-    }
-
-    public string ToString()
-    {
-        return "Inputed " + action.ToString();
-    }
+    
 }
