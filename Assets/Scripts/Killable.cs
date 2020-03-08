c7afae37ce02801c3abc447c195003be51f195d6
srikar7a <45158579+srikar7a@users.noreply.github.com>
2020-03-08 14:57:10 -0400

    killable

---
+++ Assets/Scripts/Killable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killable : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            PlayerRespawn pr = other.gameObject.GetComponent<PlayerRespawn>();
+            pr.respawn();
+        }
+    }
+}
