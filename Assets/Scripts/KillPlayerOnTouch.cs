d3f6aae77a166bdb09b918a4eb2a6c83d024e197
Michael Brazeau <michaelbrazeau@253-3.synergydsl.com>
2020-03-08 22:53:13 -0400

    kill block in needs to be added to scenes in project settings to work

---
+++ Assets/Scripts/KillPlayerOnTouch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class KillPlayerOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        }
+    }
+}
