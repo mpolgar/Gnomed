76da27684ee8bb3df9ba37f3299f3e9523b1ece8
srikar7a <45158579+srikar7a@users.noreply.github.com>
2020-03-09 01:26:49 -0400

    checkpoint

---
+++ Assets/Scripts/CheckpointUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointUpdate : MonoBehaviour
+{
+    public bool entered = false; 
+    void OnTriggerEnter2D(Collider2D other){
+        if((other.tag == "Player") && (!entered)){
+            entered = true;
+            PlayerRespawn pr = other.gameObject.GetComponent<PlayerRespawn>();
+            pr.updateCheckpoint(this.transform.position);           
+        }
+    }
+}
